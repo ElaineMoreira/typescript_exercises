bccd861613c197a0f8a034706911aa87da62447f
Elaine <elainefelipemoreira@gmail.com>
2022-02-17 19:36:50 -0300

    cria um script para subir servidor e instala nodemon

--- src/server.ts
+++ src/server.ts
@@ -12,4 +12,6 @@ app.listen(3000, () => console.log(`Sobe servidor na porta 3000! :)`))
 // node nao interpreta typescript, logo para subir o servidor, tem que compilar o codigo e rodar o codigo compilado
 // 1- configura o typescript - npx tsc --init crinado tsconfig.ts
 // 2- abra ele e mude as configuracoes desejadas
+// 3- para compilar npx tsc 
+// 4- para executar o arquivo e subir o servidor basta dar node server.js
 
