4db71411c6b2b94653f620282001371a00ba9cee
Elaine <elainefelipemoreira@gmail.com>
2022-02-17 19:30:06 -0300

    Adiciona modificacoes para fazer compilacao

--- src/server.ts
+++ src/server.ts
@@ -9,3 +9,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => console.log(`Sobe servidor na porta 3000! :)`))
+// node nao interpreta typescript, logo para subir o servidor, tem que compilar o codigo e rodar o codigo compilado
+// 1- configura o typescript - npx tsc --init crinado tsconfig.ts
+// 2- abra ele e mude as configuracoes desejadas
+
