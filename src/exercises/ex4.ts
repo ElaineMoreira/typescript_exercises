32987c7eb8f32e15d1a94122529c60b5c0cee6d5
Elaine <elainefelipemoreira@gmail.com>
2022-02-18 09:33:33 -0300

    treinando funcoes de areas geometricas em typescript

---
+++ src/exercises/ex4.ts
@@ -0,0 +1,15 @@
+/** funcao para calcular a área do triângulo */
+
+export function triangle(base: number, height: number): number {
+  return (base * height)/2;
+};
+/** funcao para calcular a área do quadrado */
+
+export function square(side: number): number {
+  return side ** 2;
+};
+/** funcao para calcular a área do retangulo */
+
+export function rectangle(base: number, height: number): number {
+  return base * height
+};
