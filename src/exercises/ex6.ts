d5e9458e915def0d8559e993b22db17df5302e9d
Elaine <elainefelipemoreira@gmail.com>
2022-02-18 10:06:49 -0300

    aprendendo comportamento de arrays e tuplas

---
+++ src/exercises/ex6.ts
@@ -0,0 +1,11 @@
+/**Tipos de coleção */
+
+// arrays
+
+let names: string[] = ["Elaine", "Diego", "Floki", "Stark"];
+
+// tuplas
+
+let fullName: [string, string] = ["Elaine", "Diego"];
+let person: [string, number] = ["Elaine", 32];
+let car: [string, string, number] = ["chevrolet", "camaro", 27];
