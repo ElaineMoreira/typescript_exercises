46118f4f70e22dcdaeef9f70476156d224d9b3d0
Elaine <elainefelipemoreira@gmail.com>
2022-02-18 10:21:00 -0300

    aprendendo type union-uniao de tipo

---
+++ src/exercises/ex8.ts
@@ -0,0 +1,9 @@
+/**Type Unions(uniao de tipos) */
+
+export function retornarCPF(cpf: number | string ) {
+  console.log("Seu CPF é" + cpf)
+};
+
+retornarCPF(096234567);
+// saida
+// Seu CPF é 096234567
