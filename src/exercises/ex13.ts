d7f2fe28b4cab0ec7eddb6a9066787216a6e257e
Elaine <elainefelipemoreira@gmail.com>
2022-02-18 14:53:17 -0300

    Genericos com interface

---
+++ src/exercises/ex13.ts
@@ -0,0 +1,14 @@
+/**genericos com interfaces e classes */
+
+interface ProcessIdentity<T, U>{
+  (value: T, message: U): T
+};
+
+function processIdentity<T, U>(value: T, message: U): T {
+  console.log(message);
+  return value;
+}
+
+let processor: ProcessIdentity<number, string> = processIdentity
+let returnNumber = processor(100, "Olá");
+let returnString = processor("Olá", 100); /**da erro por causa da disposicao do tipo */
